25aaa67bef621ce90da853fdab2f032f7770e041
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2024-04-02 16:46:46 +0200

    add pagination component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import FetchData from './Data/FetchData'
 import { DataContext } from './Data/DataContext';
 
+import Pagination from '@mui/material/Pagination'
+
 
 function App() {
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState()
+  const [paginationNumb, setPaginationNumb] = useState()
+  const [readyTable, setReadyTable] = useState()
 
-  console.log(data, 'test');
+
+  useEffect(() =>{
+  
+    if(data && data.data){
+      printData()
+    } else{
+      console.log('err');
+    }
+  
+  },[data])
+
+
+ const handlePageChange = (e, page) =>{
+  setPaginationNumb(page)
+  
+ }
+
+ const printData = () =>{
+  const listOfTags =  data.data.items.map((item) =>{
+    return(
+      <span key={item.name}>{item.name}</span>
+    )
+   })
+   
+   setReadyTable(listOfTags)
+ }
+
+ 
+
 
   return (
     <DataContext.Provider value={data}>
         <FetchData
           setData={setData}
+          paginationNumb={paginationNumb}
         />
-
+       <Pagination
+        onChange={handlePageChange}
+        count={10} />
+        <div>
+       {readyTable}
+        </div>
     </DataContext.Provider>
   )
 }
--- src/Data/FetchData.jsx
+++ src/Data/FetchData.jsx
@@ -4,25 +4,40 @@ import { useEffect } from 'react'
 import axios from 'axios'
 
 
-const FetchData = ({setData, page = 1}) =>{
+const FetchData =   ({setData, paginationNumb=1 }) =>{
    
+  
+
+
+ 
   useEffect(() =>{
-     getData()
-  },[])
 
-  const getData = (page) =>{
-    axios({
-      method: 'get',
-      url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&sparkline=false',
-      params: {page: page}
-  })
-    .then(res => setData(res.data))
-    .catch(err => console.error(err))
+  
 
-  }
+    const fetchRespone = async () =>{
+      try{
+        const responseFetch = await axios({
+          method: 'get',
+          url: 'https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow',
+          params: {page: paginationNumb, pagesize: 10}
+      })
+        .then(res => setData(res))
+      }catch(err){
+          console.log(err);
+      }
+    }
+
+   
+
+    setTimeout(() => fetchRespone(), 1000);
+  
+  
+  },[paginationNumb])
+
+  
 
   return (
-    <div>FetchData</div>
+    <div></div>
   )
 }
 
